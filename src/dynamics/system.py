11b58b7e545888cda2da3a77715d7897ed31f663
melisande-c <melisande.croft@fht.org>
2024-09-13 11:39:42 +0200

    feat: allow more params customisable

--- src/dynamics/system.py
+++ src/dynamics/system.py
@@ -1,4 +1,3 @@
-
 import numpy as np
 from numpy.typing import NDArray, ArrayLike
 
@@ -6,9 +5,19 @@ rng = np.random.default_rng()
 
 
 def gen_simulation_data(
-    n_particles: int, n_frames: int, velocity_noise_std=0.0001
+    n_particles: int,
+    n_frames: int,
+    speed_mean: float = 0.01,
+    speed_std: float = 0.0001,
+    positive_velocity_ratio=0.5,
+    velocity_noise_std=0.0001,
 ) -> NDArray:
-    initial_velocities = gen_initial_velocities(n_particles)
+    initial_velocities = gen_initial_velocities(
+        n_particles,
+        mean=speed_mean,
+        std=speed_std,
+        positive_ratio=positive_velocity_ratio,
+    )
     initial_positions = gen_initial_positions(n_particles)
     velocities = gen_velocities(
         initial_velocities, n_frames, noise_std=velocity_noise_std
@@ -21,25 +30,23 @@ def gen_positions(initial_positions: NDArray, velocities: NDArray) -> NDArray:
     return initial_positions + np.cumsum(velocities, axis=0)
 
 
-# TODO need to account for particles starting out of frame?
-# - see what it looks like once lifetimes are added
+# TODO: offset start time instead of allowing out of line bounds ?
 def gen_initial_positions(n_particles: int) -> NDArray:
     return rng.uniform(size=n_particles, low=-0.5, high=1.5)
 
 
 def gen_initial_velocities(
-    n_particles: int, mean: float = 0.01, std: float = 0.0001, positive_ratio=0.5
+    n_particles: int, mean: float, std: float, positive_ratio
 ) -> NDArray:
-    velocities = rng.normal(size=n_particles, loc=mean, scale=std)
-    random_signs = rng.choice(
+    speeds = rng.normal(size=n_particles, loc=mean, scale=std)
+    # TODO: catch negative speeds
+    directions = rng.choice(
         a=[-1, 1], size=n_particles, p=[1 - positive_ratio, positive_ratio]
     )
-    return np.array(velocities) * np.array(random_signs)
+    return np.array(speeds) * np.array(directions)
 
 
-def gen_velocities(
-    initial_velocities: NDArray, n_frames: int, noise_std=0.0001
-) -> NDArray:
+def gen_velocities(initial_velocities: NDArray, n_frames: int, noise_std) -> NDArray:
     velocities = np.tile(initial_velocities, (n_frames, 1))
     noise = rng.normal(size=velocities.shape, loc=0, scale=noise_std)
     return velocities + noise
