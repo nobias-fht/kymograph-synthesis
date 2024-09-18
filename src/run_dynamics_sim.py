be7f13379821a2fe3ea216289286deb2cd57aad0
melisande-c <melisande.croft@fht.org>
2024-09-18 17:27:41 +0200

    expl: param changes

--- src/run_dynamics_sim.py
+++ src/run_dynamics_sim.py
@@ -5,14 +5,15 @@ if __name__ == "__main__":
     from kymograph_synthesis import dynamics
 
     particle_positions = dynamics.system.gen_simulation_data(
-        50,
+        30,
         100,
         speed_mean=1 / 100,
-        speed_std=0.1 / 100,
+        speed_std=1 / 100,
         velocity_noise_std=0.002,
-        state_change_prob=0,
+        state_change_prob=0.05,
         positive_prob=0.5,
-        stopped_prob=0,
+        stopped_prob=0.5,
+        expected_lifetime=0.5
     )
     for i in range(particle_positions.shape[1]):
         plt.plot(particle_positions[:, i], np.arange(particle_positions.shape[0]))
