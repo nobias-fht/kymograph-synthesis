14a31ec22a57850d5b647335aa9f59e916c4f268
melisande-c <melisande.croft@fht.org>
2024-11-06 18:12:06 +0100

    refac: remove old renderers

--- src/kymograph_synthesis/render/space_time_objects/__init__.py
+++
@@ -1,9 +0,0 @@
-__all__ = [
-    "ParticleSystem",
-    "SpaceTimeObject",
-    "StaticSimplexNoise"
-]
-
-from .particle_system import ParticleSystem
-from .space_time_object import SpaceTimeObject
-from .static_simplex_noise import StaticSimplexNoise
