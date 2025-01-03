2c74076529b8c64d7874e46e3eb9da8d6fff558a
melisande-c <melisande.croft@fht.org>
2025-01-03 17:51:41 +0000

    feat: add default factory to all dynamics params

--- src/kymograph_synthesis/params/dynamics_params.py
+++ src/kymograph_synthesis/params/dynamics_params.py
@@ -1,3 +1,5 @@
+from typing import Any, Literal
+from typing_extensions import Self
 from math import isclose
 
 from pydantic import (
@@ -5,9 +7,10 @@ from pydantic import (
     BaseModel,
     ConfigDict,
     field_validator,
+    model_validator,
     PositiveInt,
     PositiveFloat,
-    NonNegativeFloat
+    NonNegativeFloat,
 )
 import numpy as np
 
@@ -18,63 +21,134 @@ TransitionMatrixType = dict[
 ]
 
 
+def speed_variance_from_normal_std(
+    speed_mode: PositiveFloat, normal_std: PositiveFloat
+) -> PositiveFloat:
+    normal_mean = np.log(speed_mode) + normal_std**2
+    return (np.exp(normal_std**2) - 1) * np.exp(2 * normal_mean + normal_std**2)
+
+
+def retro_var_default_factory(normal_std: PositiveFloat):
+    def inner(data: dict[str, Any]) -> PositiveFloat:
+        return speed_variance_from_normal_std(
+            speed_mode=data["retro_mode"], normal_std=normal_std
+        )
+
+    return inner
+
+
+def antero_var_default_factory(normal_std: PositiveFloat):
+    def inner(data: dict[str, Any]) -> PositiveFloat:
+        return speed_variance_from_normal_std(
+            speed_mode=data["antero_mode"], normal_std=normal_std
+        )
+
+    return inner
+
+
+def transition_matrix_default_factory(data: dict[str:Any]) -> TransitionMatrixType:
+    values = np.random.rand(3, 3)
+    # unidirectional particles do not transition
+    if data["particle_behaviour"] == "unidirectional":
+        values[(0, 0, 1, 2, 2), (1, 2, 1, 0, 1)] = 0
+    # normalise to sum to 1
+    values = np.divide(values, values.sum(axis=1).reshape(-1, 1), where=values != 0)
+    states = [
+        MotionStateCollection.ANTEROGRADE,
+        MotionStateCollection.STATIONARY,
+        MotionStateCollection.ANTEROGRADE,
+    ]
+    transition_matrix = {}
+    for i, from_state in enumerate(states):
+        transition_matrix[from_state] = {}
+        for j, to_state in enumerate(states):
+            transition_matrix[from_state][to_state] = values[i, j]
+    return transition_matrix
+
+
 class DynamicsParams(BaseModel):
 
     model_config = ConfigDict(validate_assignment=True)
 
-    n_steps: PositiveInt = Field(description="The number of simulation steps in the dynamics simulation.")
+    n_steps: PositiveInt = Field(default_factory=lambda: np.random.randint(64, 128))
     """The number of simulation steps in the dynamics simulation."""
 
-    particle_density: PositiveFloat
+    particle_density: PositiveFloat = Field(
+        default_factory=lambda: np.random.uniform(1, 12)
+    )
     """Average particle density. Units are number of particles per path."""
 
-    retro_mode: PositiveFloat
+    retro_mode: PositiveFloat = Field(default_factory=lambda: np.random.uniform(0.5, 6))
     """
     Mode of particle speed in the retrograde direction, modeled by a log-normal 
     distribution. Units are percentage of path per simulation time step.
     """
 
-    retro_var: PositiveFloat
+    retro_var: PositiveFloat = Field(
+        default_factory=retro_var_default_factory(normal_std=0.1)
+    )
     """
     Variance of particle speed in the retrograde direction, modeled by a log-normal 
     distribution. Units are percentage of path per simulation time step.
     """
 
-    antero_mode: PositiveFloat
+    antero_mode: PositiveFloat = Field(
+        default_factory=lambda: np.random.uniform(0.5, 6)
+    )
     """
     Mode of particle speed in the anterograde direction, modeled by a log-normal 
     distribution. Units are percentage of path per simulation time step.
     """
 
-    antero_var: PositiveFloat
+    antero_var: PositiveFloat = Field(
+        default_factory=antero_var_default_factory(normal_std=0.1)
+    )
     """
     Variance of particle speed in the anterograde direction, modeled by a log-normal 
     distribution.
     """
 
-    noise_var: PositiveFloat
+    noise_var: PositiveFloat = Field(default_factory=lambda: np.random.uniform(0, 0.4))
     """
     Variance of the velocity noise, modeled by a Gaussian distribution. Units are 
     percentage of path per simulation time step.
     """
 
-    intensity_mode: PositiveFloat
+    intensity_mode: PositiveFloat = Field(
+        default_factory=lambda: np.random.uniform(200, 400)
+    )
     """
     Mode of the particle intensities, modeled by a log-normal distribution.
     """
 
-    intensity_var: PositiveFloat
+    intensity_var: PositiveFloat = Field(
+        default_factory=lambda: np.random.uniform(0, 30**2)
+    )
     """Variance of the particle intensities, modeled by a log-normal distribution."""
 
-    intensity_halflife_mode: PositiveFloat
+    intensity_halflife_mode: PositiveFloat = Field(
+        default_factory=lambda data: np.random.uniform(
+            data["n_steps"] * 0.5, data["n_steps"] * 1.5
+        )
+    )
     """Mode of particle intensity half-life, modeled by a log-normal distribution."""
 
-    intensity_halflife_var: PositiveFloat
+    intensity_halflife_var: PositiveFloat = Field(
+        default_factory=lambda data: np.random.uniform(
+            data["n_steps"] * 0.5 / 10, data["n_steps"] * 1.5 / 10
+        )
+        ** 2
+    )
     """
     Variance of the paticle intensity half-life, modeled by a log-normal distribution.
     """
 
-    transition_matrix: TransitionMatrixType
+    particle_behaviour: Literal["unidirectional", "bidirectional"]
+
+    # TODO: generate transition matrix from unidirectional label
+    transition_matrix: TransitionMatrixType = Field(
+        default_factory=transition_matrix_default_factory
+    )
     """
     Matrix containing the probabilities for a particle to transition from one state to 
     another. First index denotes the particles current state and the second denotes the 
@@ -96,3 +170,25 @@ class DynamicsParams(BaseModel):
                     f"{probability_sum}, probabilities must sum to 1."
                 )
         return transition_matrix
+
+    @model_validator(mode="after")
+    def validate_particle_behaviour(self) -> Self:
+        states = [
+            MotionStateCollection.ANTEROGRADE,
+            MotionStateCollection.STATIONARY,
+            MotionStateCollection.ANTEROGRADE,
+        ]
+        transition_matrix_array = np.array(
+            [
+                [self.transition_matrix[from_state][to_state] for to_state in states]
+                for from_state in states
+            ]
+        )
+        if (self.particle_behaviour == "unidirectional") and (
+            transition_matrix_array[(0, 0, 1, 2, 2), (1, 2, 1, 0, 1)] == 0
+        ).any():
+            raise ValueError(
+                "Particle behviour label 'unidirectional' does not match transition "
+                "matrix"
+            )
+        return self
