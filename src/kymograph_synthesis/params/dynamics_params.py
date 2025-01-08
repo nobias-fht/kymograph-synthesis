b9a9e910c5a6a261f6639d5177a9473a40edcf84
melisande-c <melisande.croft@fht.org>
2025-01-08 17:59:26 +0100

    feat: add time_delta and path_length to DynamicsParams; docs

--- src/kymograph_synthesis/params/dynamics_params.py
+++ src/kymograph_synthesis/params/dynamics_params.py
@@ -21,6 +21,8 @@ TransitionMatrixType = dict[
 ]
 
 
+# TODO: this is so confusing ... just define original distribution in terms of
+#   normal_std
 def speed_variance_from_normal_std(
     speed_mode: PositiveFloat, normal_std: PositiveFloat
 ) -> PositiveFloat:
@@ -72,97 +74,136 @@ def transition_matrix_default_factory(data: dict[str:Any]) -> TransitionMatrixTy
 
 
 class DynamicsParams(BaseModel):
+    """
+    Parameters for the dynamics simulation of particles.
+
+    Parameters
+    ----------
+    n_steps: int, optional
+        The number of steps in the simulation. If unspecified, a random integer between
+        64 and 128 will be chosen.
+    time_delta: float, default=1.0
+        The time difference between simulation steps, the units are seconds [s].
+    path_length: float, optional
+        The length of the path the particles will be observed moving along, the units
+        are micrometers [um]. If unspecified, a random number between 10 and 20 will be
+        chosen.
+    particle_density: float, optional
+        Average particle density, the units are particles per micrometer [#/um]. If
+        unspecified, a random number between 0.1 and 1 will be chosen.
+    retro_mode: float, optional
+        Mode of particle speed in the retrograde direction, modeled by a log-normal
+        distribution. Units are micrometers per second [um/s]. If unspecified, a
+        random number between 0.2 and 1 is chosen.
+    retro_var: float, optional
+        Variance of particle speed in the retrograde direction, modeled by a log-normal
+        distribution. Units are micrometers per second [um/s]. If not speciefied, the
+        value will be calculated such that the underlying normal distribution has
+        a standard deviation of 0.1.
+    antero_mode: float, optional
+        Mode of particle speed in the anterograde direction, modeled by a log-normal
+        distribution. Units are micrometers per second [um/s]. If unspecified, a
+        random number between 0.2 and 1 is chosen.
+    antero_var: float, optional
+        Variance of particle speed in the anterograde direction, modeled by a
+        log-normal distribution. Units are micrometers per second [um/s]. If not
+        speciefied, the value will be calculated such that the underlying normal
+        distribution has a standard deviation of 0.1.
+    noise_var: float, optional
+        Variance of the velocity noise, modeled by a Gaussian distribution. Units are
+        micrometers per second [um/s]. If unspecified a random number between 0 and
+        0.4 will be chosen.
+    fluorophores_per_particle_mode: float, optional
+        Mode of the number of fluorophores per particle, modeled by a log-normal
+        distribution. If unspecified, a random number between 200 and 400 will be
+        chosen.
+    fluorophores_per_particle_var: float, optional
+        Variance of the number of fluorophores per particle, modeled by a log-normal
+        distribution. If unspecified, a random number between 0 and 900 will be chosen.
+    fluorophore_halflife_mode: float, optional
+        Mode of the flurophore_halflife of each particle, modeled by a log-normal
+        distribution.
+    fluorophore_halflife_var: float, optional
+        Variance of the flurophore_halflife of each particle, modeled by a log-normal
+        distribution.
+    particle_behaviour: {"unidirectional", "bidirectional"}, optional
+        Label that can be used to specify the particle behaviour. This will determine 
+        the construction of the transition matrix.
+    transition_matrix: dict[MotionStateCollection, dict[MotionStateCollection, float]]
+        Matrix containing the probabilities for a particle to transition from one state 
+        to another. The first index denotes the particle's current state, and the 
+        second denotes the state it can transition to.
+    seed: int
+        A seed for the dynamics simulation for reproducibility.
+    """
+    # TODO: 
+    #   - fluorophore_halflife_mode default values docs
+    #   - fluorophore_halflife_var default values docs
 
     model_config = ConfigDict(validate_assignment=True, validate_default=True)
 
     n_steps: PositiveInt = Field(default_factory=lambda: np.random.randint(64, 128))
-    """The number of simulation steps in the dynamics simulation."""
+
+    time_delta: PositiveFloat = 1.0
+
+    path_length: PositiveFloat = Field(
+        default_factory=lambda: np.random.uniform(10, 20)
+    )
 
     particle_density: PositiveFloat = Field(
-        default_factory=lambda: np.random.uniform(1, 12)
+        default_factory=lambda: np.random.uniform(0.1, 1)
     )
-    """Average particle density. Units are number of particles per path."""
 
-    retro_mode: PositiveFloat = Field(default_factory=lambda: np.random.uniform(0.5, 6))
-    """
-    Mode of particle speed in the retrograde direction, modeled by a log-normal 
-    distribution. Units are percentage of path per simulation time step.
-    """
+    retro_mode: PositiveFloat = Field(default_factory=lambda: np.random.uniform(0.2, 1))
 
     retro_var: PositiveFloat = Field(
         default_factory=retro_var_default_factory(normal_std=0.1)
     )
-    """
-    Variance of particle speed in the retrograde direction, modeled by a log-normal 
-    distribution. Units are percentage of path per simulation time step.
-    """
 
     antero_mode: PositiveFloat = Field(
-        default_factory=lambda: np.random.uniform(0.5, 6)
+        default_factory=lambda: np.random.uniform(0.2, 1)
     )
-    """
-    Mode of particle speed in the anterograde direction, modeled by a log-normal 
-    distribution. Units are percentage of path per simulation time step.
-    """
 
     antero_var: PositiveFloat = Field(
         default_factory=antero_var_default_factory(normal_std=0.1)
     )
-    """
-    Variance of particle speed in the anterograde direction, modeled by a log-normal 
-    distribution.
-    """
 
     noise_var: PositiveFloat = Field(default_factory=lambda: np.random.uniform(0, 0.4))
-    """
-    Variance of the velocity noise, modeled by a Gaussian distribution. Units are 
-    percentage of path per simulation time step.
-    """
 
-    intensity_mode: PositiveFloat = Field(
+    fluorophores_per_particle_mode: PositiveFloat = Field(
         default_factory=lambda: np.random.uniform(200, 400)
     )
-    """
-    Mode of the particle intensities, modeled by a log-normal distribution.
-    """
 
-    intensity_var: PositiveFloat = Field(
+    fluorophores_per_particle_var: PositiveFloat = Field(
         default_factory=lambda: np.random.uniform(0, 30**2)
     )
-    """Variance of the particle intensities, modeled by a log-normal distribution."""
 
-    intensity_halflife_mode: PositiveFloat = Field(
+    # TODO: probably not very realistic, particles should have the same bleaching rate
+    fluorophore_halflife_mode: PositiveFloat = Field(
         default_factory=lambda data: np.random.uniform(
-            data["n_steps"] * 0.5, data["n_steps"] * 1.5
+            data["n_steps"] * data["time_delta"] * 0.5,
+            data["n_steps"] * data["time_delta"] * 1.5,
         )
     )
-    """Mode of particle intensity half-life, modeled by a log-normal distribution."""
 
-    intensity_halflife_var: PositiveFloat = Field(
+    fluorophore_halflife_var: PositiveFloat = Field(
         default_factory=lambda data: np.random.uniform(
-            data["n_steps"] * 0.5 / 10, data["n_steps"] * 1.5 / 10
+            data["n_steps"] * data["time_delta"] * 0.5 / 10,
+            data["n_steps"] * data["time_delta"] * 1.5 / 10,
         )
         ** 2
     )
-    """
-    Variance of the paticle intensity half-life, modeled by a log-normal distribution.
-    """
 
     particle_behaviour: Literal["unidirectional", "bidirectional"] = Field(
         default_factory=lambda: np.random.choice(["unidirectional", "bidirectional"])
     )
 
-    # TODO: generate transition matrix from unidirectional label
     transition_matrix: TransitionMatrixType = Field(
         default_factory=transition_matrix_default_factory
     )
-    """
-    Matrix containing the probabilities for a particle to transition from one state to 
-    another. First index denotes the particles current state and the second denotes the 
-    state it can transition to.
-    """
 
-    seed: int = Field(default_factory=lambda: np.random.randint(2**63))
+
+    seed: PositiveInt = Field(default_factory=lambda: np.random.randint(2**63))
 
     @field_validator("transition_matrix")
     @classmethod
