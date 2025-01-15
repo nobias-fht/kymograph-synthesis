ec0e877792c273686d440d1046b0b62dd166925b
melisande-c <melisande.croft@fht.org>
2025-01-15 19:12:18 +0100

    feat: type hints

--- src/kymograph_synthesis/generate/generate_dynamics.py
+++ src/kymograph_synthesis/generate/generate_dynamics.py
@@ -1,9 +1,12 @@
 import numpy as np
+from numpy.typing import NDArray
 from ..params import DynamicsParams
 from ..dynamics import create_particle_simulators, run_dynamics_simulation
 
 
-def generate_dynamics(params: DynamicsParams):
+def generate_dynamics(
+    params: DynamicsParams,
+) -> tuple[NDArray[np.float_], NDArray[np.float_]]:
     dynamics_rng = np.random.default_rng(seed=params.seed)
     particles = create_particle_simulators(
         **params.model_dump(exclude=["seed", "particle_behaviour"]), rng=dynamics_rng
@@ -11,4 +14,4 @@ def generate_dynamics(params: DynamicsParams):
     particle_positions, particle_fluorophore_count, particle_states = (
         run_dynamics_simulation(params.n_steps, particles)
     )
-    return particle_positions, particle_fluorophore_count, particle_states 
+    return particle_positions, particle_fluorophore_count, particle_states
