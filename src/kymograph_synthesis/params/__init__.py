99deb959dbc72e72979e3ad316ca44bc6807ea56
melisande-c <melisande.croft@fht.org>
2025-01-16 10:58:43 +0100

    feat: generate image time series

--- src/kymograph_synthesis/params/__init__.py
+++ src/kymograph_synthesis/params/__init__.py
@@ -2,10 +2,11 @@ __all__ = [
     "RenderingParams",
     "DynamicsParams",
     "KymographParams",
+    "ImagingParams",
     "Params"
 ]
 
-from .params import Params
+from .params import Params, ImagingParams
 from .render_params import RenderingParams
 from .dynamics_params import DynamicsParams
 from .kymograph_params import KymographParams
