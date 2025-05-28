84cb62092b6d29a1795909aff870334a67f69662
melisande-c <milly.croft@gmail.com>
2025-05-28 15:30:13 +0200

    feat: add instance ground truth func; in new ground truth func module

---
+++ src/kymograph_synthesis/ground_truth/funcs.py
@@ -0,0 +1,87 @@
+import numpy as np
+from numpy.typing import NDArray
+
+
+def generate_state_ground_truth(
+    particle_positions: NDArray[np.float_],
+    particle_states: NDArray[np.int_],
+    n_spatial_values: int,
+):
+    n_steps = particle_positions.shape[0]
+
+    kymograph_gt = np.zeros((n_steps, n_spatial_values, 3), dtype=np.uint8)
+    particle_positions_pixels = particle_positions * n_spatial_values
+    mid_points = (particle_positions_pixels[1:] + particle_positions_pixels[:-1]) / 2
+    for p in range(particle_positions.shape[1]):
+        for t in range(n_steps):
+            if t == 0:
+                x0 = particle_positions_pixels[t, p]
+            else:
+                x0 = mid_points[t - 1, p]
+
+            x1 = particle_positions_pixels[t, p]
+
+            if t == n_steps - 1:
+                x2 = particle_positions_pixels[t, p]
+            else:
+                x2 = mid_points[t, p]
+
+            d1 = np.sign(np.floor(x1) - np.floor(x0))
+            d2 = np.sign(np.floor(x2) - np.floor(x1))
+            indices = np.concatenate(
+                [
+                    np.array([int(np.floor(x0))]),
+                    np.array([int(np.floor(x1))]),
+                    np.arange(np.floor(x0), np.floor(x1), d1 if d1 != 0 else 1).astype(
+                        int
+                    ),
+                    np.arange(np.floor(x1), np.floor(x2), d2 if d2 != 0 else 1).astype(
+                        int
+                    ),
+                ]
+            )
+            in_bounds = np.logical_and((0 <= indices), (indices < n_spatial_values))
+            kymograph_gt[t, indices[in_bounds], particle_states[t, p]] = 1
+
+    return kymograph_gt
+
+
+def generate_instance_ground_truth(
+    particle_positions: NDArray[np.float_],
+    n_spatial_values: int,
+):
+    n_steps, n_particles = particle_positions.shape
+    kymograph_gt = np.zeros(((n_steps, n_spatial_values, n_particles)), dtype=np.uint8)
+    particle_positions_pixels = particle_positions * n_spatial_values
+    mid_points = (particle_positions_pixels[1:] + particle_positions_pixels[:-1]) / 2
+    for p in range(particle_positions.shape[1]):
+        for t in range(n_steps):
+            if t == 0:
+                x0 = particle_positions_pixels[t, p]
+            else:
+                x0 = mid_points[t - 1, p]
+
+            x1 = particle_positions_pixels[t, p]
+
+            if t == n_steps - 1:
+                x2 = particle_positions_pixels[t, p]
+            else:
+                x2 = mid_points[t, p]
+
+            d1 = np.sign(np.floor(x1) - np.floor(x0))
+            d2 = np.sign(np.floor(x2) - np.floor(x1))
+            indices = np.concatenate(
+                [
+                    np.array([int(np.floor(x0))]),
+                    np.array([int(np.floor(x1))]),
+                    np.arange(np.floor(x0), np.floor(x1), d1 if d1 != 0 else 1).astype(
+                        int
+                    ),
+                    np.arange(np.floor(x1), np.floor(x2), d2 if d2 != 0 else 1).astype(
+                        int
+                    ),
+                ]
+            )
+            in_bounds = np.logical_and((0 <= indices), (indices < n_spatial_values))
+            kymograph_gt[t, indices[in_bounds], p] = 1
+    return kymograph_gt
