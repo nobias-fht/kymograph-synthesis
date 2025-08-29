a3b912acca6d3545984087b0a7aa2150b9d63d21
melisande-c <milly.croft@gmail.com>
2025-08-29 18:25:29 +0200

    feat: improve gt gen + refactor

--- src/kymograph_synthesis/ground_truth.py
+++ src/kymograph_synthesis/ground_truth.py
@@ -1,65 +1,27 @@
+from typing import Generator
+
 import numpy as np
 from numpy.typing import NDArray
 
-# TODO: fix this mess
+
 def generate_state_ground_truth(
     particle_positions: NDArray[np.float_],
     particle_states: NDArray[np.int_],
     n_spatial_values: int,
     line_thickness: int,
 ):
-    if (line_thickness - 1) % 2 != 0:
-        raise ValueError("Only odd values for line thickness are supported.")
-    padding = (line_thickness - 1) // 2
     n_steps = particle_positions.shape[0]
 
     kymograph_gt = np.zeros((n_steps, n_spatial_values, 3), dtype=np.uint8)
     particle_positions_pixels = particle_positions * n_spatial_values
-    mid_points = (particle_positions_pixels[1:] + particle_positions_pixels[:-1]) / 2
     for p in range(particle_positions.shape[1]):
-        for t in range(n_steps):
-            if t == 0:
-                x0 = particle_positions_pixels[t, p]
-            else:
-                x0 = mid_points[t - 1, p]
-
-            x1 = particle_positions_pixels[t, p]
-
-            if t == n_steps - 1:
-                x2 = particle_positions_pixels[t, p]
-            else:
-                x2 = mid_points[t, p]
-
-            d1 = np.sign(np.floor(x1) - np.floor(x0))
-            d2 = np.sign(np.floor(x2) - np.floor(x1))
-            indices = np.concatenate(
-                [
-                    np.arange(
-                        int(np.floor(x0)) - padding,
-                        int(np.floor(x0)) + padding + 1,
-                        dtype=int,
-                    ),
-                    np.arange(
-                        int(np.floor(x1)) - padding,
-                        int(np.floor(x1)) + padding + 1,
-                        dtype=int,
-                    ),
-                    np.arange(
-                        np.floor(x0) - d1 * padding,
-                        np.floor(x1) + d1 * padding,
-                        d1 if d1 != 0 else 1,
-                        dtype=int,
-                    ),
-                    np.arange(
-                        np.floor(x1) - d2 * padding,
-                        np.floor(x2) + d2 * padding,
-                        d2 if d2 != 0 else 1,
-                        dtype=int,
-                    ),
-                ]
-            )
-            in_bounds = np.logical_and((0 <= indices), (indices < n_spatial_values))
-            kymograph_gt[t, indices[in_bounds], particle_states[t, p]] = 1
+        indices_gen = _calc_line_indices(
+            particle_positions_pixels[:, p],
+            n_spatial_values,
+            thickness=line_thickness,
+        )
+        for t, indices in enumerate(indices_gen):
+            kymograph_gt[t, indices, particle_states[t, p]] = 1
 
     return kymograph_gt
 
@@ -67,40 +29,57 @@ def generate_state_ground_truth(
 def generate_instance_ground_truth(
     particle_positions: NDArray[np.float_],
     n_spatial_values: int,
-    line_thickness: int,
+    line_thickness: float,
 ):
-    if (line_thickness - 1) % 2 != 0:
-        raise ValueError("Only odd values for line thickness are supported.")
-    padding = (line_thickness - 1) // 2
-
     n_steps, n_particles = particle_positions.shape
     kymograph_gt = np.zeros(((n_steps, n_spatial_values, n_particles)), dtype=np.uint8)
     particle_positions_pixels = particle_positions * n_spatial_values
-    mid_points = (particle_positions_pixels[1:] + particle_positions_pixels[:-1]) / 2
     for p in range(particle_positions.shape[1]):
-        for t in range(n_steps):
-            if t == 0:
-                x0 = particle_positions_pixels[t, p]
-            else:
-                x0 = mid_points[t - 1, p]
-
-            x1 = particle_positions_pixels[t, p]
-
-            if t == n_steps - 1:
-                x2 = particle_positions_pixels[t, p]
-            else:
-                x2 = mid_points[t, p]
-
-            d1 = np.sign(np.floor(x1) - np.floor(x0))
-            d2 = np.sign(np.floor(x2) - np.floor(x1))
-            indices = np.concatenate(
-            [
-                np.arange(int(np.floor(x0))-padding, int(np.floor(x0))+padding+1, dtype=int),
-                np.arange(int(np.floor(x1))-padding, int(np.floor(x1))+padding+1, dtype=int),
-                np.arange(np.floor(x0)-d1*padding, np.floor(x1)+d1*padding, d1 if d1 != 0 else 1, dtype=int),
-                np.arange(np.floor(x1)-d2*padding, np.floor(x2)+d2*padding, d2 if d2 != 0 else 1, dtype=int)
-            ]
+        indices_gen = _calc_line_indices(
+            particle_positions_pixels[:, p],
+            n_spatial_values,
+            thickness=line_thickness,
         )
-            in_bounds = np.logical_and((0 <= indices), (indices < n_spatial_values))
-            kymograph_gt[t, indices[in_bounds], p] = 1
+        for t, indices in enumerate(indices_gen):
+            kymograph_gt[t, indices, p] = 1
     return kymograph_gt
+
+
+def _calc_line_indices(
+    particle_pixel_positions: NDArray[np.floating],
+    n_spatial_values: int,
+    thickness: float = 1,
+) -> Generator[NDArray[np.int_], None, None]:
+    mid_points = (particle_pixel_positions[1:] + particle_pixel_positions[:-1]) / 2
+    half_width = thickness - 1
+    n_steps = len(particle_pixel_positions)
+    prev_indices: list[NDArray[np.int_]] | None = None
+    for t in range(n_steps - 1):
+        x0 = particle_pixel_positions[t]
+        x1 = mid_points[t]
+        x2 = particle_pixel_positions[t + 1]
+
+        d = np.sign(x2 - x1)
+        if d == 0:
+            d = 1
+
+        current_indices: list[NDArray[np.int_]] = []
+        for points in [(x0, x1), (x1, x2)]:
+            p0 = np.round(points[0] - half_width * d)
+            p1 = np.round(points[1] + half_width * d)
+            if p1 == p0:
+                p1 += d
+
+            indices = np.arange(p0, p1, d).astype(int)
+            in_bounds = np.logical_and((0 <= indices), (indices < n_spatial_values))
+            current_indices.append(indices[in_bounds])
+
+        if prev_indices is None:
+            yield current_indices[0]
+        else:
+            yield np.concatenate([prev_indices[-1], current_indices[0]])
+
+        prev_indices = current_indices
+
+    if prev_indices is not None:
+        yield prev_indices[-1]
