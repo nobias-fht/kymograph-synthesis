aa1adc2bf45c1d179f80473d60dffbb771dd9938
Melisande Croft <63270704+melisande-c@users.noreply.github.com>
2025-08-29 18:38:51 +0200

    Feature(Ground Truth Generation): Line thickness option (#5)
    * feat: line thickness option when generating ground truth
    
    * feat: improve gt gen + refactor
    
    * feat(config): add line thickness to gt config & model_dump params in pipeline

--- src/kymograph_synthesis/params/ground_truth_params/func_params.py
+++ src/kymograph_synthesis/params/ground_truth_params/func_params.py
@@ -8,6 +8,7 @@ from .collection import GroundTruthFuncCollection
 class StateGroundTruth(BaseModel):
 
     name: Literal[GroundTruthFuncCollection.STATE] = GroundTruthFuncCollection.STATE
+    line_thickness: float = 1
 
 
 class InstanceGroundTruth(BaseModel):
@@ -15,7 +16,7 @@ class InstanceGroundTruth(BaseModel):
     name: Literal[GroundTruthFuncCollection.INSTANCE] = (
         GroundTruthFuncCollection.INSTANCE
     )
-
+    line_thickness: float = 1
 
 GroundTruthFuncParams = Annotated[
     Union[StateGroundTruth, InstanceGroundTruth], Field(discriminator="name")
