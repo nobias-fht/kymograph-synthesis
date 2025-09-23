ed243986f1fbe9ab8c0586293ee27a9e28356a08
melisande-c <milly.croft@gmail.com>
2025-09-23 16:28:15 +0200

    feat: parameter to project ground truth speeds to xy plane, make default

--- src/kymograph_synthesis/ground_truth.py
+++ src/kymograph_synthesis/ground_truth.py
@@ -3,13 +3,21 @@ from typing import Generator
 import numpy as np
 from numpy.typing import NDArray
 
+from kymograph_synthesis.render.static_path import PiecewiseQuadraticBezierPath
+
 
 def generate_state_ground_truth(
     particle_positions: NDArray[np.float_],
     particle_states: NDArray[np.int_],
     n_spatial_values: int,
+    path_points: list[tuple[float, float, float]],
+    *,
     line_thickness: int,
+    project_to_xy: bool,
 ):
+    if project_to_xy:
+        particle_positions = _project_positions_to_xy(particle_positions, path_points)
+
     n_steps = particle_positions.shape[0]
 
     kymograph_gt = np.zeros((n_steps, n_spatial_values, 3), dtype=np.uint8)
@@ -29,8 +37,13 @@ def generate_state_ground_truth(
 def generate_instance_ground_truth(
     particle_positions: NDArray[np.float_],
     n_spatial_values: int,
-    line_thickness: float,
+    path_points: list[tuple[float, float, float]],
+    *,
+    line_thickness: int,
+    project_to_xy: bool,
 ):
+    if project_to_xy:
+        particle_positions = _project_positions_to_xy(particle_positions, path_points)
     n_steps, n_particles = particle_positions.shape
     kymograph_gt = np.zeros(((n_steps, n_spatial_values, n_particles)), dtype=np.uint8)
     particle_positions_pixels = particle_positions * n_spatial_values
@@ -43,6 +56,51 @@ def generate_instance_ground_truth(
         for t, indices in enumerate(indices_gen):
             kymograph_gt[t, indices, p] = 1
     return kymograph_gt
+
+
+def _project_positions_to_xy(
+    particle_positions: NDArray[np.float_],
+    path_points: list[tuple[float, float, float]],
+) -> NDArray[np.float_]:
+    static_path = PiecewiseQuadraticBezierPath(
+        [np.array(point) for point in path_points]
+    )
+    projected_path_points = []
+    for point in path_points:
+        point = (0, *point[1:])
+        projected_path_points.append(np.array(point))
+    projected_path = static_path
+
+    particle_coords = static_path(particle_positions).squeeze()
+    velocity = particle_coords[1:] - particle_coords[:-1]
+
+    adjacent = (velocity[..., 2] ** 2 + velocity[..., 1] ** 2) ** 0.5
+    theta = np.arctan2(velocity[..., 0], adjacent)
+    speed = np.linalg.norm(velocity, axis=-1)
+    projected_speed = speed * np.cos(theta)
+    direction = np.sign(particle_positions[1:] - particle_positions[:-1])
+
+    # have to rescale positions because of inaccurate calculations for out of bounds movement
+    initial_positions = particle_positions[0]
+    new_position = np.tile(initial_positions, (particle_positions.shape[0], 1))
+    new_position[1:] = new_position[1:] + np.cumsum(
+        projected_speed * direction / projected_path.length(), axis=0
+    )
+    in_bounds = np.logical_and(0 <= particle_positions, particle_positions <= 1)
+    for p in range(particle_positions.shape[1]):
+        if not in_bounds[:, p].any():
+            continue
+        target_min = particle_positions[:, p][in_bounds[:, p]].min()
+        target_max = particle_positions[:, p][in_bounds[:, p]].max()
+        min_ = new_position[:, p][in_bounds[:, p]].min()
+        max_ = new_position[:, p][in_bounds[:, p]].max()
+
+        new_position[:, p] = (
+            (new_position[:, p] * (target_max - target_min) / (max_ - min_))
+            - min_
+            + target_min
+        )
+    return new_position
 
 
 def _calc_line_indices(
